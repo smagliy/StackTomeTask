5fa1cc01e620783419d8eba0e005c801b9d001e7
Kateryna Smahlii <smaglijkata2002@gmail.com>
2023-10-12 17:56:00 +0300

    Added a service for writing parsing information from the model to file

---
+++ src/main/scala/com/crawler/services/write/CSVWriteService.scala
@@ -0,0 +1,36 @@
+package com.crawler.services.write
+
+import java.io.FileWriter
+
+import org.apache.commons.csv.{CSVFormat, CSVPrinter}
+
+import org.json4s._
+import org.json4s.jackson.Serialization.write
+
+import com.crawler.models._
+import com.crawler.services.log.LogService
+import com.crawler.services.write.utils.FilesService
+
+object CSVWriteService {
+  implicit val formats: Formats = DefaultFormats
+  private val csvFormat = CSVFormat.DEFAULT.withHeader("priority","id","name","ranks",
+    "domain","reviews","categories","latest_review","latest_reviews_count","monthly_visits")
+
+  def writeCompaniesToCsv(companies: List[Companies], filePath: String): Unit = {
+    LogService.logger.info(s"Trying to write the results of list companies in the ${filePath}")
+    FilesService.createOrClearFile(filePath)
+    val fileWriter = new FileWriter(filePath)
+    val csvPrinter = new CSVPrinter(fileWriter, csvFormat)
+
+    try {
+      companies.foreach { company =>
+        csvPrinter.printRecord(company.priority, company.id, company.name, company.ranks,
+          company.domain, company.reviews, company.categories.name, write(company.latestReviews.latestReview),
+          company.latestReviews.latestReviewCount, company.monthlyVisits)
+      }
+    } finally {
+      fileWriter.close()
+      csvPrinter.close()
+    }
+  }
+}
