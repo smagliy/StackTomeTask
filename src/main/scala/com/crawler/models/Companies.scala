8b66806e8ff8c2cacfd33a0ae4fc2dad0cfd3f90
Kateryna Smahlii <smaglijkata2002@gmail.com>
2023-10-24 15:12:20 +0300

    Add update method for models

--- src/main/scala/com/crawler/models/Companies.scala
+++ src/main/scala/com/crawler/models/Companies.scala
@@ -4,4 +4,9 @@ case class Companies(priority: Int, id: String, name: String,
                      ranks: Double, domain: String, reviews: String,
                      categories: Categories, latestReviews: LatestReviews, monthlyVisits: Int = 0) extends Base {
   override def toString: String = super.toString
+
+  def update(updaterLatestReviews: LatestReviews, updaterMonthlyVisits: Int = 0): Companies={
+    Companies(priority, id, name, ranks, domain, reviews, categories,
+      updaterLatestReviews, updaterMonthlyVisits)
+  }
 }
--- src/main/scala/com/crawler/models/LatestReviews.scala
+++ src/main/scala/com/crawler/models/LatestReviews.scala
@@ -1,5 +1,9 @@
 package com.crawler.models
 
-case class LatestReviews(latestReview: Map[String, Any], latestReviewCount: Int) extends Base {
+case class LatestReviews(latestReview: Map[String, Any], listOfCommenterIDs: List[String]) extends Base {
   override def toString: String = super.toString
+
+  def update(newLatestReview: Map[String, Any], newListOfCommenterIDs: List[String]): LatestReviews = {
+    LatestReviews(newLatestReview, (listOfCommenterIDs ++ newListOfCommenterIDs).distinct)
+  }
 }
