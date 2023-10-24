7de2551b733cb25bcfe4eef575d854a89f452f9d
Kateryna Smahlii <smaglijkata2002@gmail.com>
2023-10-24 18:12:39 +0300

    Remove unnecessary actions to upgrade the Company

--- src/main/scala/com/crawler/models/Companies.scala
+++ src/main/scala/com/crawler/models/Companies.scala
@@ -5,8 +5,8 @@ case class Companies(priority: Int, id: String, name: String,
                      categories: Categories, latestReviews: LatestReviews, monthlyVisits: Int = 0) extends Base {
   override def toString: String = super.toString
 
-  def update(updaterLatestReviews: LatestReviews, updaterMonthlyVisits: Int = 0): Companies={
+  def update(updaterLatestReviews: LatestReviews): Companies={
     Companies(priority, id, name, ranks, domain, reviews, categories,
-      updaterLatestReviews, updaterMonthlyVisits)
+      updaterLatestReviews, monthlyVisits)
   }
 }
--- src/main/scala/com/crawler/services/update/CompanyUpdater.scala
+++ src/main/scala/com/crawler/services/update/CompanyUpdater.scala
@@ -2,17 +2,15 @@ package com.crawler.services.update
 
 import com.crawler.models._
 import com.crawler.services.parse.trustpilot.LatestReviewsParser
-import com.crawler.services.parse.vstat.TrafficParser
 import com.crawler.services.log.LogService
 
 import scala.util.Try
 
 class CompanyUpdater  {
-  def update(company: Companies): Companies ={
+  def update(company: Companies): Companies = {
     LogService.logger.info(s"Updating ${company.domain}")
     val updatedLatestReviews = updateLatestReviews(company.latestReviews, company.id)
-    val updatedTraffic = new TrafficParser(List(company.domain)).parse().head
-    company.update(updatedLatestReviews, updatedTraffic)
+    company.update(updatedLatestReviews)
   }
 
   private def updateLatestReviews(oldLatestReviews: LatestReviews, id: String): LatestReviews = {
