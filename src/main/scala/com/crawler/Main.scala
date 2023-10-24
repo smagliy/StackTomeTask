52f445b3b44778d0c3d12f9a4c50650c5198095c
Kateryna Smahlii <smaglijkata2002@gmail.com>
2023-10-24 18:16:36 +0300

    Integration of futures in updating information

--- src/main/scala/com/crawler/Main.scala
+++ src/main/scala/com/crawler/Main.scala
@@ -6,20 +6,35 @@ import com.crawler.services.CrawlerPipeline
 import com.crawler.services.update.CompanyUpdater
 import com.crawler.services.log.LogService
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 object Main {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   private val crawler = CrawlerPipeline
+  private var companiesList: List[Companies] = crawler.parseGeneralListOfCompanies
   private val modelUpdater = new CompanyUpdater
 
+  private def generateFutureList: Future[List[Companies]] = {
+    val updateFutures: List[Future[Companies]] = companiesList.map { company =>
+      Future { modelUpdater.update(company) }
+    }
+    Future.traverse(updateFutures)(identity)
+  }
+
   def main(args: Array[String]): Unit = {
-    val initialCompanies: List[Companies] = crawler.parseGeneralListOfCompanies
-    crawler.sortAndWriteListOfCompanies(initialCompanies)
+    crawler.sortAndWriteListOfCompanies(companiesList)
     while (true) {
-      LogService.logger.info("Waiting 5 min for updating info")
+      LogService.logger.info("Waiting 5 min for updating")
       Thread.sleep(5.minutes.toMillis)
-      val updatedCompanies = initialCompanies.map(company => modelUpdater.update(company))
-      crawler.sortAndWriteListOfCompanies(updatedCompanies)
+      generateFutureList.onComplete {
+        case Success(updatedCompanies) =>
+          companiesList = updatedCompanies.distinct
+          crawler.sortAndWriteListOfCompanies(companiesList)
+        case Failure(exception) =>
+          LogService.logger.error("Error during company updates: " + exception.getMessage)
+      }
     }
   }
 }
